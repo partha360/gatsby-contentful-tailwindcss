f9e2a43bc6d1718313b452ee9b30e13d918be2b8
partha360 <partha@finiteloop.io>
2018-05-21 11:38:42 +0530

    Add a test case for flags display

--- src/components/test/lang-select.test.js
+++ src/components/test/lang-select.test.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { FormattedMessage, IntlProvider, addLocaleData } from 'react-intl';
 import { configure, shallow } from 'enzyme';
+import Link from 'gatsby-link';
 import 'intl';
 import en from 'react-intl/locale-data/en';
 import 'intl/locale-data/jsonp/en';
 import de from 'react-intl/locale-data/de';
 import 'intl/locale-data/jsonp/de';
 import Adapter from 'enzyme-adapter-react-16';
+import languages from '../../data/languages';
 import LangSelect from '../lang-select';
+import DE from '../icons/de';
+import US from '../icons/us';
+
+function flag(lang) {
+  if (lang === 'de') {
+    return <DE />;
+  }
+  return <US />;
+}
 
 configure({ adapter: new Adapter() });
 
@@ -47,11 +58,21 @@ describe('Header with Navigation', () => {
 
   it('should render respective language flag icons', () => {
     expect(
-      shallow(
-        <IntlProvider locale="en" messages={i18nMessages}>
-          <FormattedMessage id="selectLanguage" />
-        </IntlProvider>
-      )
+      langSet.map(lang => {
+        const newLink = lang.link.replace(
+          lang.langKey,
+          languages.langKey[lang.langKey]
+        );
+        return (
+          <Link
+            className="px-1"
+            to={newLink}
+            key={languages.langKey[lang.langKey]}
+          >
+            {flag(lang.langKey)}
+          </Link>
+        );
+      })
     );
   });
 });
